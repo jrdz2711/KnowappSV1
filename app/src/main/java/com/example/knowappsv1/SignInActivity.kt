1fa721a98bc8d9ad1bdaf4fbe2626b92cd649e18
jrdz2711 <j.rodriguez2016ng@gmail.com>
2019-05-08 23:53:40 -0600

    Initial commit

---
+++ app/src/main/java/com/example/knowappsv1/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.knowappsv1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
---
+++ app/src/main/java/com/example/knowappsv1/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.example.knowappsv1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import org.jetbrains.anko.startActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (FirebaseAuth.getInstance().currentUser == null)
+            startActivity<LoginActivity>()
+        else
+            startActivity<MainActivity>()
+        finish()
+    }
+}
